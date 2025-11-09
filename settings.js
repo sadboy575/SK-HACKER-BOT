ed07f7775d7c3b9358627a87b187069f4c474143
SK HACKER <kasamsezehrelagteho@gmail.com>
2025-11-09 12:04:17 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,15 @@
+const settings = {
+  'packname': "SK-BOTV1",
+  'author': '‎',
+  'botName': "  𓆩𓆪SK-𝖡𝖮𝖳V1",
+  'botOwner': "𝐎𝐖𝐍𝐄𝐑 : SK HACKER",
+  'ownerNumber': "923086375560",
+  'giphyApiKey': 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  'commandMode': "public",
+  'maxStoreMessages': 0x14,
+  'storeWriteInterval': 0x2710,
+  'description': "This is a bot for managing group commands and automating tasks.",
+  'version': "1.0.0",
+  'updateZipUrl': 'https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip'
+};
+module.exports = settings;
